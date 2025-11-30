046f29267b96596e96c731a8cffb76fcec598990
Pepijn <pepijndevue@hotmail.com>
2025-11-30 15:40:30 +0200

    Fixed import ...

--- src/pages/doc_ankertjes.py
+++ src/pages/doc_ankertjes.py
@@ -1,6 +1,6 @@
 """Ankertjes documentation page."""
 from pathlib import Path
-import streamlit_utils as st_utils
+import src.streamlit_utils as st_utils
 
 # Get the path to the markdown file
 base_dir = Path(__file__).parent.parent.parent.resolve()
