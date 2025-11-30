a7c7f861c6b2dd7108bfb33b6733051c65eb51b4
Pepijn <pepijndevue@hotmail.com>
2025-11-30 15:31:47 +0200

    Relative import bug

--- src/pages/doc_readme.py
+++ src/pages/doc_readme.py
@@ -1,6 +1,6 @@
 """README documentation page."""
 from pathlib import Path
-from .markdown_page import render_markdown_page
+from markdown_page import render_markdown_page
 
 # Get the path to the markdown file
 base_dir = Path(__file__).parent.parent.parent.resolve()
