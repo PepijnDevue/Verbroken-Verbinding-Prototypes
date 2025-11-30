f58630d3dd070a135a945f949b6edefcc88a9382
Pepijn <pepijndevue@hotmail.com>
2025-11-30 15:57:46 +0200

    Documentation pages now work

--- src/pages/docs/doc_data_geinformeerde_feedback.py
+++ src/pages/docs/doc_data_geinformeerde_feedback.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import src.streamlit_utils as st_utils
 
 # Get the path to the markdown file
-base_dir = Path(__file__).parent.parent.parent.resolve()
+base_dir = Path(__file__).parent.parent.parent.parent.resolve()
 markdown_file = base_dir / "docs" / "data_geïnformeerde_feedback.md"
 
 st_utils.render_markdown_page(str(markdown_file))
